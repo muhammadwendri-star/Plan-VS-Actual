12011eea42e91f79cbfbf6c62bb1be67d1404bf9
Muhammad Wendri <emailkamu@example.com>
2026-04-27 09:02:42 +0700

    Upload plan and actual projects

---
+++ Plan/README.md
@@ -0,0 +1,5 @@
+# 360-Test
+Viewer panorama 360° menggunakan Marzipano.
+- Navigasi: tombol panah & zoom
+- Toggle fullscreen & autorotate
+- Multi-scene: Panorama2, Panorama3, Panorama4
---
+++ Plan/data.js
@@ -0,0 +1,344 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-view-1",
+      "name": "View 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06319237734951955,
+          "pitch": 0.2999099156323908,
+          "rotation": 0,
+          "target": "1-view-2"
+        },
+        {
+          "yaw": -0.9582642314268437,
+          "pitch": 0.3297868949784899,
+          "rotation": 0,
+          "target": "3-view-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8668327110268201,
+          "pitch": 0.42409071592785885,
+          "title": "Basketball",
+          "text": "Sport Surfacing&nbsp;"
+        },
+        {
+          "yaw": 0.0920464099640963,
+          "pitch": 0.6006903883889692,
+          "title": "Concrete Paving",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6651266161336782,
+          "pitch": 1.001056857798396,
+          "title": "Grated Drain",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7844390358655282,
+          "pitch": 0.35227715359645373,
+          "title": "P Building",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-view-2",
+      "name": "View 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8785975214503132,
+          "pitch": 0.2678365134972882,
+          "rotation": 4.71238898038469,
+          "target": "2-view-3"
+        },
+        {
+          "yaw": -3.127558957504778,
+          "pitch": 0.24553823109383544,
+          "rotation": 0,
+          "target": "0-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-view-3",
+      "name": "View 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.884582158430069,
+          "pitch": 0.6167106080592983,
+          "rotation": 0,
+          "target": "3-view-4"
+        },
+        {
+          "yaw": -1.7939864676071409,
+          "pitch": 0.2119246944176716,
+          "rotation": 0,
+          "target": "4-view-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3637195969093625,
+          "pitch": 0.2566374663921991,
+          "title": "Cargo Net Seating",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.9323956651731446,
+          "pitch": 0.40749321730284294,
+          "title": "Basketball",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.1647362886696762,
+          "pitch": 0.11775460218594169,
+          "title": "Totempole",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.8406812436713587,
+          "pitch": 0.3736320181389665,
+          "title": "Standing Basin",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-view-4",
+      "name": "View 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8470295951881015,
+          "pitch": 0.373216842423421,
+          "rotation": 4.71238898038469,
+          "target": "0-view-1"
+        },
+        {
+          "yaw": -1.1263870467582606,
+          "pitch": 0.19242310681067742,
+          "rotation": 1.5707963267948966,
+          "target": "4-view-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-view-5",
+      "name": "View 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2922741453532325,
+          "pitch": 0.1659207943025951,
+          "rotation": 0,
+          "target": "5-view-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-view-6",
+      "name": "View 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.226647936311915,
+          "pitch": 0.23238121360575192,
+          "rotation": 0,
+          "target": "4-view-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-view-7",
+      "name": "View 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
